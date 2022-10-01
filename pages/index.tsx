ba3e689efa1e4954031ccf6cbff1799d6341a12e
Job Breedveld <jobbreedveld@gmail.com>
2022-10-01 13:43:55 +0200

    feat/install-next

---
+++ pages/index.tsx
@@ -0,0 +1,26 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Enternet.dev</title>
+        <meta name="description" content="Enternet.dev marketing website" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>Welcome to Enternet!</h1>
+
+        <p className={styles.description}>
+          We are currently under construction
+        </p>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
